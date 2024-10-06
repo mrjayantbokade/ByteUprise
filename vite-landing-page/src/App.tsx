de1185ca9504e6341008f32fe565136b225bb894
Jayant Bokade <mrjayant81@gmail.com>
2024-10-06 10:17:22 +0530

    Project initiated

---
+++ vite-landing-page/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Button } from "@/components/ui/button";
+import Particles from "@/components/ui/particles";
+
+function App() {
+
+  return (
+    <>
+      <div className={`bg-black h-screen w-screen text-white`}>
+        <Particles/>
+      </div>
+    </>
+  )
+}
+
+export default App
